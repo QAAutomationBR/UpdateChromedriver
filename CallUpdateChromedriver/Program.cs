66292a97c31133927641a4b42c9a755645af8f41
Marcos Antonio Prado Silva <marcosprado@outlook.com>
2021-08-04 16:05:05 -0300

    Initial Commit

---
+++ CallUpdateChromedriver/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UpdateChromeDriver;
+
+namespace TesteConsulta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Este projeto foi criado a fim de demostração de utilização da dll
+
+            VersaoChromeDriver versao = new VersaoChromeDriver();
+
+            Console.WriteLine($"O chrome driver correto para sua versão do chrome é {versao.versaoChromeDriver()}");
+
+
+            Console.ReadLine();
+                
+                
+
+
+        }
+    }
+}
---
+++ UpdateChromedriver/versionador.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Windows.Forms;
+
+namespace UpdateChromeDriver
+{
+    public class VersaoChromeDriver
+    {
+
+        public string versaoChromeDriver()
+        {
+
+            string exeFolder = Path.GetDirectoryName(Application.ExecutablePath);
+
+            string chrome = consultaultimaversao();
+
+            string ChromeDriverDirectory = exeFolder + $@"\{chrome}\";
+
+
+            while (!consultaarquivo(ChromeDriverDirectory))
+            { }
+
+
+            return ChromeDriverDirectory;
+
+        }
+
+        private string consultaultimaversao()
+        {
+            var versao = Consultaversao().Split('.');
+            string chrome;
+            using (var client = new WebClient())
+            {
+
+                var URL = $"https://chromedriver.storage.googleapis.com/LATEST_RELEASE_{versao[0]}.{versao[1]}.{versao[2]}";
+
+
+
+                chrome = client.DownloadString(URL);
+
+
+
+
+            }
+
+            return chrome;
+        }
+
+        private string Consultaversao()
+        {
+            object path = Registry.GetValue(@"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe", "", null);
+            if (path != null)
+                return FileVersionInfo.GetVersionInfo(path.ToString()).FileVersion;
+            return string.Empty;
+        }
+
+
+
+        private bool consultaarquivo(string arquivo)
+        {
+            FileInfo file = new FileInfo(arquivo + @"\chromedriver.exe");
+            if (file.Exists)
+            {
+                return true;
+            }
+            else
+            {
+                using (var client = new WebClient())
+                {
+                    var versao = consultaultimaversao();
+
+                    var URL = $"https://chromedriver.storage.googleapis.com/{versao}/chromedriver_win32.zip";
+
+                    string caminho = Path.GetDirectoryName(Application.ExecutablePath);
+
+                    client.DownloadFile(URL, $@"{caminho}\{versao}.zip");
+                    FileInfo novo = new FileInfo($@"{caminho}\{versao}.zip");
+
+                    while (!novo.Exists)
+                    {
+
+                    }
+
+                    ZipFile.ExtractToDirectory($@"{caminho}\{versao}.zip", $@"{caminho}\{versao}\");
+
+
+                    string LATESTFolder = $@"{caminho}\LATEST\";
+                    System.IO.DirectoryInfo diLATEST = new DirectoryInfo(LATESTFolder);
+
+                    if (!diLATEST.Exists)
+                    {
+                        diLATEST.Create();
+                        //Directory.CreateDirectory(LATESTFolder);
+                    }
+                    else
+                    {
+                        foreach (FileInfo fileItem in diLATEST.GetFiles())
+                        {
+                            file.Delete();
+                        }
+
+                    }
+
+                    
+
+                    novo = new FileInfo($@"{caminho}\{versao}\chromedriver.exe");
+                    while (!novo.Exists)
+                    {
+
+                    }
+
+                    File.Copy($@"{caminho}\{versao}\chromedriver.exe", $@"{LATESTFolder}\chromedriver.exe");
+
+                    File.Delete($@"{caminho}\{versao}.zip");
+
+
+                }
+                return false;
+
+
+            }
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+
+}
